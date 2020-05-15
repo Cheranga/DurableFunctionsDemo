a0fde997d4cc79ba8f0cb13105d5820bde436bce
Cheranga Hatangala <cheranga@gmail.com>
2020-05-16 07:18:27 +1000

    Add project files.

---
+++ DurableFunctionsDemo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DurableFunctionsDemo;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace DurableFunctionsDemo
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var services = builder.Services;
+            //
+            // TODO:
+            // Register dependencies
+            //
+        }
+    }
+}
